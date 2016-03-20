fc78a8a14f41a97f55420313d76a2e8511d94668
Marcode777 <marc102202@hotmail.com>
2016-03-20 11:53:16 -0400

    installed dependencies, modified seed.js file

--- public/seed.js
+++ public/seed.js
@@ -10,13 +10,31 @@ var Comment = require('./model/comment');
 
 // to create a User
 var user1 = new User({
-  name: 'Mean Stacker',
-  money: 100000,
-  password: "stackthemup",
-  collected: ['Gold', 'Silver', 'Bitcoin']
+  name: 'Mean Precious Metal Gram Collector',
+  money: 1000,
+  password: "stackthegrams",
+  collected: ['GoldGram', 'SilverGram', 'Bitcoin']
 });
 
 // inside of saving the user 
 user1.save(function(err){
   if(err) return(err);
-})
+
+// for creating comments within user save
+var comment1 = new Comment({
+  commentMsg: "I'm stacking precious grams!",
+  _owner: user1.id,
+  itemLink:"Precious Gram1"
+});
+
+var comment2 = new Comment ({
+  commentMsg:"I like precious grams!",
+  _owner: user1.id,
+  itemLink: "Precious Gram2"
+});
+
+comment1.save(function(err){
+  if (err) return (err);
+});
+
+}); 
